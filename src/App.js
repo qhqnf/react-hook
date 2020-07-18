95b8e4276f5fabe99b1e2b3009174600a31af3c1
root <didguscjf95@naver.com>
2020-07-18 18:32:25 +0900

    useInput, useTabs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React, { useState } from "react";
+import { useInput, useTabs } from "./hooks/StateHooks";
+
+const apiContent = [
+  {
+    tab: "Section 1",
+    content: "I'm section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm section 2",
+  },
+  {
+    tab: "Section 3",
+    content: "I'm section 3",
+  },
+];
 
 const App = () => {
   const [item, setItem] = useState(1);
   const incrementItem = () => setItem(item + 1);
   const decrementItem = () => setItem(item - 1);
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr. ", maxLen);
+  const { currentItem, changeItem } = useTabs(0, apiContent);
   return (
     <div className="App">
-      <span>{item}</span>
-      <button onClick={incrementItem}>plus</button>
-      <button onClick={decrementItem}>minus</button>
+      <div>
+        <h1>Hello! Number:{item}</h1>
+        <button onClick={incrementItem}>+1</button>
+        <button onClick={decrementItem}>-1</button>
+      </div>
+      <div>
+        <input placeholder="Name" {...name}></input>
+      </div>
+      <div>
+        {apiContent.map((section, index) => (
+          <button onClick={() => changeItem(index)}>{section.tab}</button>
+        ))}
+        <div>{currentItem.content}</div>
+      </div>
     </div>
   );
 };
---
+++ src/hooks/StateHooks.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
+export const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
+};
